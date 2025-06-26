24b5d2267a687e878aa8f417a9702bc5c6c4dcdf
Manjundhar <103714273+Manju025@users.noreply.github.com>
2025-06-26 15:38:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import requests
+import os
 app = Flask(__name__)
 CORS(app)  # Allow frontend access
 
@@ -26,4 +27,5 @@ def get_hospitals():
     return jsonify(response.json())
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
